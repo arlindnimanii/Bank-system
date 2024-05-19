0402c69b50514b997b98ba6d0c65f2402267756d
arlindnimanii <146958254+arlindnimanii@users.noreply.github.com>
2024-05-19 21:03:15 +0200

    Update Account.java

--- Account.java
+++ Account.java
@@ -1,1 +1,21 @@
 
+package com.example.bnaka20242;
+
+public abstract class Account {
+    protected String accNo;
+    protected String name;
+    protected String accType;
+    protected double balance;
+
+    public Account(String accNo, String name, String accType, double balance) {
+        this.accNo = accNo;
+        this.name = name;
+        this.accType = accType;
+        this.balance = balance;
+    }
+
+    public abstract void showAccount();
+    public abstract boolean search(String accNo);
+    public abstract double deposit(double amount, int feeType);
+    public abstract double withdrawal(double amount, int feeType);
+}
